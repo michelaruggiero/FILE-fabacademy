e82dc08a7695f339f0f51f9db85b43b47418933c
michela <michelaruggiero@gmail.com>
2015-05-31 21:23:46 +0200

    marcoprimo

---
+++ week 10/PHOTOSENSORE.ino
@@ -0,0 +1,20 @@
+int analogInPin = A0;
+int sensorValue = A0;
+ 
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  Serial.print("sensor = " );
+  //pinMode(6, INPUT);
+}
+ 
+void loop() {
+  digitalWrite(13,HIGH);
+  sensorValue = analogRead(A0);            
+ 
+  Serial.print("sensor = " );
+  Serial.println(sensorValue);      
+ 
+  delay(1000);
+  }
+  
