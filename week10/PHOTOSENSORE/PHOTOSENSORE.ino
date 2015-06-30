b6ff99fc70386ea937be8bb8a4f73cfca1d95a21
michela <michelaruggiero@gmail.com>
2015-06-30 18:44:40 +0200

    files

--- week10/PHOTOSENSORE/PHOTOSENSORE.ino
+++ week10/PHOTOSENSORE/PHOTOSENSORE.ino
@@ -1,14 +1,11 @@
 int sensorValue = A0;
  
 void setup() {
-  pinMode(13,OUTPUT);
   Serial.begin(9600);
-  Serial.print("sensor = " );
-  //pinMode(6, INPUT);
+
 }
  
 void loop() {
-  digitalWrite(13,HIGH);
   sensorValue = analogRead(A0);            
  
   Serial.print("sensor = " );
